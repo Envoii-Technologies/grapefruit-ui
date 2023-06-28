f1022c6c14364587bb7ec31bae829effea6777b4
FRickReich <frickreich@gmail.com>
2023-06-28 15:03:33 +0200

    Change storybook preview file

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,6 @@
-import logo from './../src/assets/logo.png';
-
 /** @type { import('@storybook/react').Preview } */
 const preview = {
   parameters: {
-    brandTitle: `<img src="/src/assets/logo.png" width="10px" height="10px"/>`,
     layout: 'centered',
     viewMode: 'docs',
     actions: { argTypesRegex: "^on[A-Z].*" },
