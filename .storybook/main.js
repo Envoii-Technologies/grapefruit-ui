daa9bb57d35df91e0810cf04cbff76ea9f893227
FRickReich <frickreich@gmail.com>
2023-06-28 15:19:56 +0200

    Change main storybook setup file

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,6 +8,8 @@ const config = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     '@storybook/addon-docs',
+    'storybook-addon-mock/register',
+    'storybook-color-picker',
     '@storybook/addon-outline',
     '@storybook/addon-viewport',
     '@storybook/addon-coverage',
@@ -16,39 +18,9 @@ const config = {
     name: "@storybook/react-vite",
     options: {},
   },
-  core: {
-    builder: {
-      name: '@storybook/builder-webpack5',
-    },
-  },
   docs: {
     autodocs: true, // see below for alternatives
     defaultName: 'Docs', // set to change the name of generated docs entries
   },
-  features: {
-    interactionsDebugger: true,
-  },
-  // webpackFinal: async (config, { configType }) => {
-  //   config.module.rules[5].oneOf.push({
-  //     test: /\.scss$/i,
-  //     use: [
-  //       'style-loader',
-  //       'css-loader',
-  //       {
-  //         loader: 'sass-loader',
-  //         options: {
-  //           api: 'modern',
-  //           sassOptions: {
-  //             outputStyle: 'expanded',
-  //           },
-  //         },
-  //       },
-  //     ],
-  //     include: path.resolve(__dirname, '../'),
-  //   });
-
-  //   
-  // },
 };
-
 export default config;
