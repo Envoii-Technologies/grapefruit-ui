7ab1f0db936a28675d427c9dffd88bb72aac5e40
FRickReich <frickreich@gmail.com>
2023-09-23 18:04:37 +0200

    fix: rewrite Page Content Component

--- src/index.js
+++ src/index.js
@@ -4,3 +4,4 @@ export { Avatar } from './components/Avatar/index.js';
 export { FormNotification } from './components/FormNotification/index.js';
 export { LoadingIndicator } from './components/LoadingIndicator/index.js';
 export { PageHeader } from './components/PageHeader/index.js';
+export { PageContent } from './components/PageContent/index.js';
