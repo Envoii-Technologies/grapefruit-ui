d607e8a31eb6c19869a4b32c675cafe0e9f6994d
FRickReich <frickreich@gmail.com>
2023-09-24 10:33:33 +0200

    refactor: change preview body of storybook module

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -5,7 +5,7 @@ import '../src/global-styles/index.scss';
 const preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
-    layout: 'fullscreen',
+    layout: 'centered',
     controls: {
       matchers: {
         color: /(background|color)$/i,
