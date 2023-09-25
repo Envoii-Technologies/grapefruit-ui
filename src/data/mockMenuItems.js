47ebf92f63c411ac5a402ca2bd5ef68002d6f77f
FRickReich <frickreich@gmail.com>
2023-09-25 15:49:25 +0200

    refactor: rewrite mock data files

--- src/data/mockMenuItems.js
+++ src/data/mockMenuItems.js
@@ -1,17 +1,98 @@
-export default {
-    owner: [
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBookOpen, faGauge, faSliders, faUsers, faBookOpen, faListCheck } from '@fortawesome/free-solid-svg-icons';
 
-    ],
+export default {
     admin: [
-        
+        {
+            title: "Dashboard",
+            icon: faGauge,
+            path: "/"
+        },
+        {
+            title: "Aufgaben",
+            icon: faListCheck,
+            path: "/tasks"
+        },
+        {
+            title: "Mitarbeiter",
+            icon: faUsers,
+            path: "/employees"
+        },
+        {
+            title: "Einstellungen",
+            icon: faSliders,
+            path: "/settings"
+        },
+        {
+            title: "Anleitungen",
+            icon: faBookOpen,
+            path: "/guides"
+        }
     ],
     manager: [
-
+        {
+            title: "Dashboard",
+            icon: faGauge,
+            path: "/"
+        },
+        {
+            title: "Aufgaben",
+            icon: faListCheck,
+            path: "/tasks"
+        },
+        {
+            title: "Mitarbeiter",
+            icon: faUsers,
+            path: "/employees"
+        },
+        {
+            title: "Anleitungen",
+            icon: faBookOpen,
+            path: "/guides"
+        }
     ],
     editor: [
-
+        {
+            title: "Dashboard",
+            icon: faGauge,
+            path: "/"
+        },
+        {
+            title: "Aufgaben",
+            icon: faListCheck,
+            path: "/tasks"
+        },
+        {
+            title: "Mitarbeiter",
+            icon: faUsers,
+            path: "/employees"
+        },
+        {
+            title: "Anleitungen",
+            icon: faBookOpen,
+            path: "/guides"
+        }
     ],
     user: [
-
+        {
+            title: "Dashboard",
+            icon: faGauge,
+            path: "/"
+        },
+        {
+            title: "Aufgaben",
+            icon: faListCheck,
+            path: "/tasks"
+        },
+        {
+            title: "Mitarbeiter",
+            icon: faUsers,
+            path: "/employees"
+        },
+        {
+            title: "Anleitungen",
+            icon: faBookOpen,
+            path: "/guides"
+        }
     ],
 }
