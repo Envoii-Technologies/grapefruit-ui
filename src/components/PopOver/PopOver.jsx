93ca00f8ba33bec7cbc0327d3a48969481a03cfc
FRickReich <frickreich@gmail.com>
2023-09-30 11:43:12 +0200

    refactor: rewrite PopOver component

--- src/components/PopOver/PopOver.jsx
+++ src/components/PopOver/PopOver.jsx
@@ -1,59 +1,66 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-import { Button } from '../Button';
-
 import './PopOver.scss';
 
 export const PopOverPosition = {
-    top: 'top',
-    bottom: 'bottom',
-    left: 'left',
-    right: 'right',
+    LEFT_TOP: 'left-top',
+    LEFT_CENTER: 'left-center',
+    LEFT_BOTTOM: 'left-bottom',
+    TOP_LEFT: 'top-left',
+    TOP_CENTER: 'top-center',
+    TOP_RIGHT: 'top-right',
+    RIGHT_TOP: 'right-top',
+    RIGHT_CENTER: 'right-center',
+    RIGHT_BOTTOM: 'right-bottom',
+    BOTTOM_LEFT: 'bottom-left',
+    BOTTOM_CENTER: 'bottom-center',
+    BOTTOM_RIGHT: 'bottom-right',
 };
 
 export const PopOver = ({
     className,
     children,
+    content,
     position,
-    options,
-    text,
+    openOnHover,
     ...props
 }) => {
-    const [isOpen, setIsOpen] = useState(false);
+    const [isVisible, setIsVisible] = useState(false);
 
-    const handleAction = (action, e) => {
-        setIsOpen(false);
+    const handleMouseEnter = () => {
+        if (openOnHover) {
+            setIsVisible(true);
+        }
+    };
 
-        action(e);
+    const handleMouseLeave = () => {
+        if (openOnHover) {
+            setIsVisible(false);
+        }
+    };
+
+    const handleClick = () => {
+        if (!openOnHover) {
+            setIsVisible(!isVisible);
+        }
     };
 
     return (
-        <div className={`PopOver ${className !== undefined ? className : ''}`}>
-            <span
-                className="PopOver__trigger"
-                onClick={() => setIsOpen(!isOpen)}
-            >
-                {children}
-            </span>
-            <div
-                className={`PopOver__content ${
-                    isOpen ? 'open' : 'closed'
-                } ${position}`}
-            >
-                {options
-                    ? options.map((option, i) => (
-                          <Button
-                              key={i}
-                              disabled={option.disabled || false}
-                              type={option.type || 'primary'}
-                              label={option.label}
-                              onClick={(e) => handleAction(option.action, e)}
-                          />
-                      ))
-                    : text
-				}
-            </div>
+        <div
+            className={`PopOver ${position} ${
+                openOnHover ? 'hoverable' : ''
+            } ${className}`}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            onClick={handleClick}
+        >
+            {children}
+            {isVisible && (
+                <div className="PopOver__content">
+                    <div className="PopOver__content-inner">{content}</div>
+                </div>
+            )}
         </div>
     );
 };
@@ -63,19 +70,31 @@ PopOver.propTypes = {
      * Custom class name of Component
      */
     className: PropTypes.string,
-    children: PropTypes.oneOfType([
-        PropTypes.arrayOf(PropTypes.element),
-        PropTypes.element,
-    ]),
-    position: PropTypes.oneOf(Object.keys(PopOverPosition)),
-    options: PropTypes.array,
-    text: PropTypes.string,
+    /**
+     * The content of the trigger element.
+     */
+    children: PropTypes.node,
+
+    /**
+     * The content to be displayed inside the popover.
+     */
+    content: PropTypes.node,
+
+    /**
+     * The positioning of the popover relative to the trigger element.
+     * Choose from predefined positions:
+     */
+    position: PropTypes.oneOf(Object.values(PopOverPosition)),
+
+    /**
+     * Determines whether the popover opens on hover (true) or on click (false).
+     */
+    openOnHover: PropTypes.bool,
 };
 
 PopOver.defaultProps = {
     className: undefined,
-    children: null,
-    position: PopOverPosition.bottom,
-    options: undefined,
-    text: 'Hello World',
+    openOnHover: false,
+    position: PopOverPosition.BOTTOM_CENTER,
+    content: null,
 };
